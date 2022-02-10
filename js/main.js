94e802197e5c2e3103bc6c41a1d7b6dddeacfb8e
J03Y004 <91735870+J03Y004@users.noreply.github.com>
2022-02-10 16:54:32 +0100

    added compare password feature

--- js/main.js
+++ js/main.js
@@ -5,10 +5,24 @@
         $(this).toggleClass("zmdi-eye zmdi-eye-off");
         var input = $($(this).attr("toggle"));
         if (input.attr("type") == "password") {
-          input.attr("type", "text");
+            input.attr("type", "text");
         } else {
-          input.attr("type", "password");
+            input.attr("type", "password");
         }
-      });
+    });
 
 })(jQuery);
+
+var password = document.getElementById("password");
+var re_password = document.getElementById("re_password");
+
+function validatePassword() {
+    if (password.value != re_password.value) {
+        re_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        re_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+re_password.onkeyup = validatePassword;
