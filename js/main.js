c305083de56a950db96fee3268805c8782c69047
J03Y004 <91735870+J03Y004@users.noreply.github.com>
2022-02-10 18:29:44 +0100

    added password validation

--- js/main.js
+++ js/main.js
@@ -16,18 +16,44 @@
 let password = document.getElementById("password");
 let re_password = document.getElementById("re_password");
 
-function validatePassword() {
-    if (password.value != re_password.value) {
-        re_password.setCustomValidity("Passwords Don't Match");
+// When the user starts to type something inside the password field
+password.onkeyup = function() {
+    // Validate capital letters
+    let upperCaseLetters = /[A-Z]/g;
+    if (myInput.value.match(upperCaseLetters)) {
+        password.setCustomValidity('');
     } else {
+        password.setCustomValidity("Must contain a capital letter");
+    }
+
+    // Validate numbers
+    let numbers = /[0-9]/g;
+    if (myInput.value.match(numbers)) {
         re_password.setCustomValidity('');
+    } else {
+        re_password.setCustomValidity("Must contain a number");
+    }
+
+    // Validate length
+    if (myInput.value.length >= 8) {
+        re_password.setCustomValidity('');
+    } else {
+        re_password.setCustomValidity("Minimum 8 character");
     }
 }
 
-password.onchange = validatePassword;
-re_password.onkeyup = validatePassword;
+function confirmPassword() {
+    if (password.value == re_password.value) {
+        re_password.setCustomValidity('');
+    } else {
+        re_password.setCustomValidity("Passwords Don't Match");
+    }
+}
 
-$("submit").click(function(){
+password.onchange = confirmPassword;
+re_password.onkeyup = confirmPassword;
+
+$("submit").click(function() {
     let name = $("name").val();
     let surname = $("surname").val();
     let email = $("email").val();
