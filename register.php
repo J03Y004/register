98a04256ea908ec08605695c5c2dbbc2ddd06b7c
J03Y004 <91735870+J03Y004@users.noreply.github.com>
2022-02-20 21:30:30 +0100

    completed registration.php

--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,78 +8,79 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Registration Completed</title>
 </head>
+
 <body>
     <?php
+    // definizione delle variabili per la query
+    $contatto_nome = $_POST['name'];
+    $contatto_cognome = $_POST['surname'];
+    $contatto_email = $_POST['email'];
+    $contatto_password = $_POST['password'];
+
+    $host = "localhost";        // nome di host
+    $db = "users";              // nome del database
+    $user = "root";             // username dell'utente in connessione
+    $password = "";             // password dell'utente
+
+    /*
+            blocco try/catch di gestione delle eccezioni
+        */
+    try {
+        // stringa di connessione al DBMS
+        $connessione = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+        // imposto l'attributo per il report degli errori
+        $connessione->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // definizione delle variabili per la query
+        $contatto_cognome = $_POST['surname'];
         $contatto_nome = $_POST['name'];
-        $contatto_cognome = $_POST['surname'];                                         
         $contatto_email = $_POST['email'];
         $contatto_password = $_POST['password'];
 
-        $HEADER = [
+        $SECRET_KEY = "QfTjWnZr4u7x!z%C*F-JaNdRgUkXp2s5";
+
+        $HEADER =  [
             'alg' => 'SHA256',
-            'type' => 'JWT'
+            'type' => 'JWT',
         ];
 
         $PAYLOAD = [
             'password' => $contatto_password
         ];
 
-        $host = "localhost";        // nome di host
-        $db = "users";              // nome del database
-        $user = "root";             // username dell'utente in connessione
-        $password = "";             // password dell'utente
-        
-        /*
-            blocco try/catch di gestione delle eccezioni
-        */
-        try {
-            // stringa di connessione al DBMS
-            $connessione = new PDO("mysql:host=$host;dbname=$db", $user, $password);
-            // imposto l'attributo per il report degli errori
-            $connessione->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // definizione delle variabili per la query
-            $contatto_cognome = $_POST['surname'];
-            $contatto_nome = $_POST['name'];
-            $contatto_email = $_POST['email'];
-            $contatto_password = $_POST['password'];
+        $HEADERJSON = json_encode($HEADER);
+        $PAYLOADJSON = json_encode($PAYLOAD);
+        $B64HEADERJSON = base64_encode($HEADERJSON);
+        $B64PAYLOADJSON = base64_encode($PAYLOADJSON);
+        $UNSIGNEDTOKEN = $B64HEADERJSON . "." . $B64PAYLOADJSON;
+        $SIGNEDTOKEN = hash_hmac('SHA256', $UNSIGNEDTOKEN, $SECRET_KEY);
+        $B64SIGNEDTOKEN = base64_encode($SIGNEDTOKEN);
+        $TOKEN = $B64HEADERJSON . "." . $B64PAYLOADJSON . "." . $B64SIGNEDTOKEN;
+        $contatto_password = $TOKEN;
 
-            $SECRET_KEY = "QfTjWnZr4u7x!z%C*F-JaNdRgUkXp2s5";
+        // preparazione della query SQL
+        $sql = $connessione->prepare("INSERT INTO FROM users (nome, cognome, email, password) VALUES (:contatto_nome, :contatto_cognome, :contatto_email, :contatto_password");
+        // bind dei parametri
+        $sql->bindParam(':contatto_cognome', $contatto_cognome, PDO::PARAM_STR, 7);
+        $sql->bindParam(':contatto_nome', $contatto_nome, PDO::PARAM_STR, 7);
+        $sql->bindParam(':contatto_email', $contatto_email, PDO::PARAM_STR, 7);
+        $sql->bindParam(':contatto_password', $contatto_password, PDO::PARAM_STR, 7);
+        $sql->execute();                                                                // esecuzione del prepared statement
+        $connessione = null;                                                            // chiusura della connessione
 
-            $HEADER =  [
-                'alg' => 'SHA256',
-                'type' => 'JWT',
-            ];
-
-            $PAYLOAD = [
-                'password' => $contatto_password 
-            ];
-
-            $HEADERJSON = json_encode($HEADER);
-            $PAYLOADJSON = json_encode($PAYLOAD);
-            $B64HEADERJSON = base64_encode($HEADERJSON);
-            $B64PAYLOADJSON = base64_encode($PAYLOADJSON);
-            $UNSIGNEDTOKEN = $B64HEADERJSON . "." . $B64PAYLOADJSON;
-            $SIGNEDTOKEN = hash_hmac('SHA256', $UNSIGNEDTOKEN, $SECRET_KEY);
-            $B64SIGNEDTOKEN = base64_encode($SIGNEDTOKEN);
-            $TOKEN = $B64HEADERJSON . "." . $B64PAYLOADJSON . "." . $B64SIGNEDTOKEN;
-            $TIPORISPOSTA = 1;
-            $RISPOSTA = $TOKEN;
-
-            // preparazione della query SQL
-            $sql = $connessione->prepare("INSERT INTO FROM users (nome, cognome, email, password) VALUES (:contatto_nome, :contatto_cognome, :contatto_email, :contatto_password");
-             // bind dei parametri
-            $sql->bindParam(':contatto_cognome', $contatto_cognome, PDO::PARAM_STR, 7);
-            $sql->bindParam(':contatto_nome', $contatto_nome, PDO::PARAM_STR, 7);
-            $sql->bindParam(':contatto_email', $contatto_email, PDO::PARAM_STR, 7);
-            $sql->bindParam(':contatto_password', $contatto_password, PDO::PARAM_STR, 7);
-            $sql->execute();                                                                // esecuzione del prepared statement
-            $connessione = null;                                                            // chiusura della connessione
-        }
-        catch(PDOException $e)
-        {
-            echo $e->getMessage();                                                          // notifica in caso di errore nel tentativo di connessione
-        }
+        echo '<section class="signup">
+                <div class="container">
+                    <div class="signup-content">
+                        <h2 class="form-title">Registration Complete</h2>
+                        <div class="form-group">
+                            <input type="submit" name="submit" id="submit" class="form-submit" value="Login"/>
+                        </div>
+                    </div>
+                </div>
+              </section>';
+    } catch (PDOException $e) {
+        echo $e->getMessage();                                                          // notifica in caso di errore nel tentativo di connessione
+    }
     ?>
 </body>
+
 </html>
